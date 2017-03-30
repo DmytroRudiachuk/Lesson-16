6b7e5c1178c31bb6f04226c15b7f5912eff2d954
keri19372 <keri19372@gmail.com>
2017-03-31 01:00:13 +0300

    added: 4 pages(with masseges), page defaulte

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var http = require("http");
+var cool = require("cool-ascii-faces");
 var port = 8080;
 
 var server = http.createServer(function(request, response){
 	console.log(request.url);
+	switch(request.url) {
+		case "/":
+			response.end("Main page");
+			break;
+		case "/about":
+			response.end("about page");
+			break;
+		case "/contact":
+			response.end("See contact");
+			break;
+		case "/cool":
+			response.end(cool());
+			break;
+		default:
+			response.end("Page 404 - Not Found :(")
+	}
 	response.end("hello from nodeJS server");
 });
 
